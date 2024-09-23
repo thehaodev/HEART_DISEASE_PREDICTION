815db7f18aef494c9da4c264a96ff07a8d588b7b
DESKTOP-Q9GD575\PC <thehaodev96@gmail.com>
2024-09-23 19:38:22 +0700

    sklearn solution

--- data_analysis.py
+++ data_analysis.py
@@ -3,12 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Read and fill empty data
 df = pd.read_csv('cleveland.csv', header=None)
 df.columns = ['age', 'sex', 'cp', 'trestbps', 'chol',
               'fbs', 'restecg', 'thalach', 'exang',
               'oldpeak', 'slope', 'ca', 'thal', 'target']
 df['target'] = df.target.map({0: 0, 1: 1, 2: 1, 3: 1, 4: 1})
-df['thal'] = df.thal.fillna(df.thal.mean)
+df['thal'] = df.thal.fillna(df.thal.mean())
 df['ca'] = df.ca.fillna(df.ca.mean())
 
 # Distribution of target vs age
