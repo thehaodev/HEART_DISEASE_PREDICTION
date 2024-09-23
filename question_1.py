2357f6b60152c80270e067648ee3ab79e924f759
DESKTOP-Q9GD575\PC <thehaodev96@gmail.com>
2024-09-23 11:08:36 +0700

    data-analysis-1

---
+++ question_1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('cleveland.csv', header=None)
+df.columns = ['age', 'sex', 'cp', 'trestbps', 'chol',
+              'fbs', 'restecg', 'thalach', 'exang',
+              'oldpeak', 'slope', 'ca', 'thal', 'target']
+df['target'] = df.target.map({0: 0, 1: 1, 2: 1, 3: 1, 4: 1})
+df['thal'] = df.thal.fillna(df.thal.mean)
+df['ca'] = df.ca.fillna(df.ca.mean())
+
+# Distribution of target vs age
+sns.set_context('paper', font_scale=1, rc={'font.size': 3, 'axes.titlesize': 15, 'axes.labelsize': 10})
+ax = sns.catplot(kind='count', data=df, x='age', hue='target', order=df['age'].sort_values().unique())
+ax.ax.set_xticks(np.arange(0, 88, 5))
+
+plt.title('Variation of Age for each target class')
+plt.show()
